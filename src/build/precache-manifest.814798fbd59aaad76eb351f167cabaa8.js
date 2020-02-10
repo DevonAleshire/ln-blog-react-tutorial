2e1f14e5fc50c6dccd4464132537d56c5b0ba38f
Devon Aleshire <devoale@gmail.com>
2020-02-09 19:11:08 -0800

    First commit

---
+++ src/build/precache-manifest.814798fbd59aaad76eb351f167cabaa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9f878f158fcd25ab0e8c1d9bb9dcc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d9d5d34989cbe7a2f6",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "cde6f0a5133f59099818",
+    "url": "/static/js/2.ad987ec5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ad987ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d9d5d34989cbe7a2f6",
+    "url": "/static/js/main.ecb13dbb.chunk.js"
+  },
+  {
+    "revision": "555f2d1f9e1952024771",
+    "url": "/static/js/runtime-main.be468d11.js"
+  }
+]);
